7675d70b9dd397a97f2c0f7a3f81fd7d9fde7b57
anbu <anbu@klyserv.com>
2018-11-19 13:50:32 +0000

    adding pynvim python3 package as well

--- setup_nvim_plugin.sh
+++ setup_nvim_plugin.sh
@@ -9,10 +9,10 @@ let $NVIM_TUI_ENABLE_CURSOR_SHAPE = 0
 
 call plug#begin('~/.local/share/nvim/plugged')
 Plug 'junegunn/vim-easy-align'
-Plug 'https://github.com/junegunn/vim-github-dashboard.git'
 Plug 'SirVer/ultisnips' | Plug 'honza/vim-snippets'
 Plug 'scrooloose/nerdtree', { 'on':  'NERDTreeToggle' }
 Plug 'itchyny/lightline.vim'
+Plug ''
 call plug#end()
 EOF
 
@@ -20,4 +20,9 @@ pip3 install neovim
 
 find /home/anbu | xargs chown anbu 
 find /home/anbu | xargs chgrp anbu 
+
+# download fuzzy finder binary
+su -c 'sh -c "$(curl -fLo /home/anbu/.local/share/nvim/plugged/fzf/bin/fzf.tar.gz --create-dirs https://github.com/junegunn/fzf-bin/releases/download/0.17.5/fzf-0.17.5-linux_amd64.tgz && cd /home/anbu/.local/share/nvim/plugged/fzf/bin && tar xf fzf.tar.gz && rm fzf.tar.gz)"' anbu
+
+# to install all the plugins listed in init.vim
 su -c 'nvim --headless +PlugInstall +qa' anbu
