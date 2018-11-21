e467c52c00c6a5f5a2b6f2f023a4294ab86fa64c
anbu <anbu@klyserv.com>
2018-11-21 22:30:51 +0000

    moving plugin config out of docker image

--- setup_nvim_plugin.sh
+++ setup_nvim_plugin.sh
@@ -1,31 +1,12 @@
 #!/bin/bash
 
-mkdir /home/anbu
+mkdir -p /home/anbu/.config/nvim
 
 su -c 'sh -c "$(curl -fLo /home/anbu/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim)"' anbu
 
 cat <<'EOF'> /home/anbu/.config/nvim/init.vim
-let $NVIM_TUI_ENABLE_CURSOR_SHAPE = 0
 
-call plug#begin('~/.local/share/nvim/plugged')
-Plug 'junegunn/vim-easy-align'
-Plug 'SirVer/ultisnips'
-Plug 'scrooloose/nerdtree', { 'on':  'NERDTreeToggle' }
-Plug 'itchyny/lightline.vim'
-Plug 'honza/vim-snippets'
-Plug 'ZeroProbability/my-vim-snippets'
-Plug 'Shougo/deoplete.nvim', { 'do': ':UpdateRemotePlugins' }
-call plug#end()
-
-let g:deoplete#enable_at_startup = 1
-
-" Trigger configuration. 
-let g:UltiSnipsExpandTrigger="<tab>"
-let g:UltiSnipsJumpForwardTrigger="<c-b>"
-let g:UltiSnipsJumpBackwardTrigger="<c-z>"
-
-" If you want :UltiSnipsEdit to split your window.
-let g:UltiSnipsEditSplit="vertical"
+source /mnt/.vim/neovim/nvim.vim
 
 EOF
 
